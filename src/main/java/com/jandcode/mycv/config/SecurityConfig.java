9909542d1ae6b8c76a2e634043a5a26ce376b364
jandcode <jandrespb.07@gmail.com>
2026-01-31 20:29:31 -0500

    fix: :rocket: add endpoint netlify to deploy

--- src/main/java/com/jandcode/mycv/config/SecurityConfig.java
+++ src/main/java/com/jandcode/mycv/config/SecurityConfig.java
@@ -45,7 +45,8 @@ public class SecurityConfig {
 
         config.setAllowedOrigins(List.of(
                 "http://localhost:5500",
-                "http://127.0.0.1:5500"
+                "http://127.0.0.1:5500",
+                "https://papaya-kelpie-3b934f.netlify.app"
         ));
 
         config.setAllowedMethods(List.of(
