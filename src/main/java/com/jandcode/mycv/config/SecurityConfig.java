b24500e6f1911222efdb16e21d19929363511e2d
jandcode <jandrespb.07@gmail.com>
2026-02-02 11:03:23 -0500

    fix: :alien: config own domain jandtocode

--- src/main/java/com/jandcode/mycv/config/SecurityConfig.java
+++ src/main/java/com/jandcode/mycv/config/SecurityConfig.java
@@ -46,7 +46,8 @@ public class SecurityConfig {
         config.setAllowedOrigins(List.of(
                 "http://localhost:5500",
                 "http://127.0.0.1:5500",
-                "https://papaya-kelpie-3b934f.netlify.app"
+                "https://jandtocode.com",
+                "https://www.jandtocode.com"
         ));
 
         config.setAllowedMethods(List.of(
